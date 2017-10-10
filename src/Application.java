e02cb4f35983c1cbf6b368c7fce6c3eebc956bd0
Jose Luis Clares <jlch.social@gmail.com>
2017-10-10 21:43:15 +0200

    testing reading files

--- src/Application.java
+++ src/Application.java
@@ -1,27 +1,28 @@
-
-
-class Machine{
-
-    public void start (){
-        System.out.println("Maching starting");
-    }
-
-}
-
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class Application {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
 
-        // ANONYMOUS CLASSES
-        Machine m1 = new Machine(){
-            @Override public void start(){
-                System.out.println("Camara starting");
-            }
-        };
+        // READ FILES
+        String fileName = "/Users/joseluisclares/Documents/EXAMPLE.txt";
+        int count =0;
+        File file = new File(fileName);
 
-        m1.start();
+        Scanner readFile = new Scanner(file);
+        count++;
+        System.out.println(count+ ": "+readFile.nextInt());
+        readFile.nextLine();
+        count++;
 
+        while (readFile.hasNextLine()){
+            System.out.println(count+ ": "+readFile.nextLine());
+            count++;
+        }
+
+        readFile.close();
     }
 
 }
