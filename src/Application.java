e56284437c0422fea92a3095cd86b038d942dca2
Jose Luis Clares <jlch.social@gmail.com>
2017-10-10 20:31:26 +0200

    testing Generics 2

--- src/Application.java
+++ src/Application.java
@@ -1,15 +1,46 @@
+import javafx.scene.Camera;
+
 import java.util.ArrayList;
+
+class Machine{
+    public String toString() {
+        return "I am a machine";
+    }
+    public void start (){
+        System.out.println("Maching starting");
+    }
+
+}
+
+class Camara extends Machine {
+    public String toString() {
+        return "I am a camara";
+    }
+    public void snap(){
+        System.out.println("snap");
+    }
+}
 
 public class Application {
 
     public static void main(String[] args) {
 
         // GENERICS
-        ArrayList <String> strList = new ArrayList<>();
-        strList.add("cat");
-        strList.add("dog");
-        String animal = strList.get(1);
-        System.out.println(animal);
+        ArrayList <Machine> list1 = new ArrayList<>();
+        list1.add(new Machine());
+        list1.add(new Machine());
+        show(list1);
 
+        ArrayList <Camara> list2 = new ArrayList<>();
+        list2.add(new Camara());
+        list2.add(new Camara());
+        show(list2);
+
+    }
+
+    private static void show(ArrayList<? super Camera> list) {
+        for (Object value: list){
+            System.out.println(value);
+        }
     }
 }
