7d81a5197a468b3c749c35cb2f2a2236ebb9ebc2
Jose Luis Clares <jlch.social@gmail.com>
2017-10-10 20:47:11 +0200

    testing Anonymous Classes

--- src/Application.java
+++ src/Application.java
@@ -1,46 +1,27 @@
-import javafx.scene.Camera;
 
-import java.util.ArrayList;
 
 class Machine{
-    public String toString() {
-        return "I am a machine";
-    }
+
     public void start (){
         System.out.println("Maching starting");
     }
 
 }
 
-class Camara extends Machine {
-    public String toString() {
-        return "I am a camara";
-    }
-    public void snap(){
-        System.out.println("snap");
-    }
-}
 
 public class Application {
 
     public static void main(String[] args) {
 
-        // GENERICS
-        ArrayList <Machine> list1 = new ArrayList<>();
-        list1.add(new Machine());
-        list1.add(new Machine());
-        show(list1);
+        // ANONYMOUS CLASSES
+        Machine m1 = new Machine(){
+            @Override public void start(){
+                System.out.println("Camara starting");
+            }
+        };
 
-        ArrayList <Camara> list2 = new ArrayList<>();
-        list2.add(new Camara());
-        list2.add(new Camara());
-        show(list2);
+        m1.start();
 
     }
 
-    private static void show(ArrayList<? super Camera> list) {
-        for (Object value: list){
-            System.out.println(value);
-        }
-    }
 }
