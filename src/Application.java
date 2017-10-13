f5bad169ba97dad900b03e8638a476cca9de12bf
Jose Luis Clares <jlch.social@gmail.com>
2017-10-13 15:16:39 +0200

    testing Try Catch with Resources

--- src/Application.java
+++ src/Application.java
@@ -1,4 +1,3 @@
-
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -9,27 +8,13 @@ public class Application {
     public static void main(String[] args){
 
         File file = new File ("/Users/joseluisclares/Documents/EXAMPLE.txtd");
-        FileReader fr = null;
 
-        try {
-            fr = new FileReader(file);
-            String str = fr.read();
-            System.out.println(str);
-        } catch (FileNotFoundException e) {
-            System.out.println("Error open the file: " + file.toString());
-            e.printStackTrace();
+        try (FileReader fr = new FileReader(file)){
+            System.out.println("File open");
         } catch (IOException e) {
-            System.out.println("Error reading file "+ file.toString());
             e.printStackTrace();
         }
 
-        try {
-            fr.close();
-        } catch (IOException e) {
-            System.out.println("Error closing file: " + file.toString());
-            e.printStackTrace();
-        } catch (NullPointerException e){
-            System.out.println("Error, file not exist: " + file.toString());
-        }
+
     }
 }
